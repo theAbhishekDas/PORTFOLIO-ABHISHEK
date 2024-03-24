704e8945ef37656903e99c448fa50b6e93599ee4
Abhishek Das <148617038+theAbhishekDas@users.noreply.github.com>
2024-03-25 03:06:29 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
           <span className='w-[30px] h-[2px] bg-primary'></span>
           Hello ! There
         </div>
-        <h1 className='h1 max-w-md mb-8'>Let's Wok Together.</h1>
+        <h1 className='h1 max-w-md mb-8'>Let's Work Together.</h1>
         <p className='subtitle max-w-[400px]'>Words bridge hearts, connecting souls in shared understanding and empathy.</p>
         </div>
         <div className='hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat'></div>
